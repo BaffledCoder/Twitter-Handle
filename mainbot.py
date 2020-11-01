0f4d57f4d62c6403f54b36e62841addc06e2f301
BaffledCoder <64409503+BaffledCoder@users.noreply.github.com>
2020-11-01 21:42:37 +0530

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -1,6 +1,15 @@
 import tweepy
 import os # operating system library
-
+from datetime import date 
+born=date(2000, 10, 11)
+def calculateAge(birthDate): 
+    today = date.today() 
+    age = today.year - birthDate.year - 
+         ((today.month, today.day) < 
+         (birthDate.month, birthDate.day)) 
+  
+    return age
+old=calculateAge(born)
 def create_api():
   consumer_key = os.getenv('consumer_key')
   consumer_secret = os.getenv('consumer_secret')
@@ -23,7 +32,7 @@ def follower_count(user):
   emoji_numbers =  {0: "0️⃣", 1: "1️⃣", 2: "2️⃣", 3: "3️⃣",
                       4: "4️⃣", 5: "5️⃣", 6: "6️⃣", 7: "7️⃣", 8: "8️⃣", 9: "9️⃣"}
 
-  uf_split = [int(i) for i in str(user.followers_count)]# Used to seperate 
+  uf_split = [int(i) for i in str(old)]# Used to seperate 
 
   emoji_followers = ''.join([emoji_numbers[j] for j in uf_split if j in emoji_numbers.keys()]) 
   return emoji_followers 
@@ -32,7 +41,7 @@ api = create_api()
 
 while True:
     user = api.get_user('_BaffledCoder')
-    api.update_profile(name=f'Shubham Sharma|{follower_count(user)} Followers')
-    print(f'Updating Twitter Name : Shubham Sharma|{follower_count(user)} Followers')
+    api.update_profile(name=f'Shubham Sharma|{old}')
+    print(f'Updating Twitter Name : Shubham Sharma|{old}')
     print('Waiting to refresh')
     time.sleep(60)
